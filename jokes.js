99e0c54720a78bd78333bb176fdc8ff14d25da75
hdh3000 <henry.hutcheson@gmail.com>
2015-09-02 10:34:26 -0700

    added  few more jokes

--- jokes.js
+++ jokes.js
@@ -1,6 +1,17 @@
 //Walks Into Bar
-var walksIntoABar = function(patron) {
-	
+var walksIntoABar = function(patron) {	
 	if(patron === "horse") { return "why the long face?"; }
 	if(patron === "two men") { return "one of them ducked"; }
 };
+
+//What did the ocean say to the beach?
+var oceanSays = function(t0Beach){
+	return "nothing, it just waved";
+};
+
+//have you had a ?
+var haveYouHad = function(item){
+	if(item === "huricaneSandy") { return "its a wattered down Manhattan"; }
+};
+
+
